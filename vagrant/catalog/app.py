77391b10323710547163b21e938c3149c9c97370
jcarter62@gmail.com <jcarter62>
2019-06-20 16:52:29 -0700

    Several minor changes

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,37 +1,12 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-HTML_WRAP = '''\
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>DB Forum</title>
-    <style>
-      h1, form { text-align: center; }
-      textarea { width: 400px; height: 100px; }
-      div.post { border: 1px solid #999;
-                 padding: 10px 10px;
-                 margin: 10px 20%%; }
-      hr.postbound { width: 50%%; }
-      em.date { color: #999 }
-    </style>
-  </head>
-  <body>
-    <h1>DB Forum</h1>
-    <form method=post>
-      <div><textarea id="content" name="content"></textarea></div>
-      <div><button id="go" type="submit">Post message</button></div>
-    </form>
-    <!-- post content will go here -->
-%s
-  </body>
-</html>
-'''
-
 @app.route("/", methods=['GET'])
 def main():
-  return HTML_WRAP
+  return render_template('main.html')
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000)
