31b126335d046d7c63d19937dd3295b3722ba5af
Jim Carter <jcarter62@gmail.com>
2019-06-20 14:10:16 -0700

    networking issues resolved

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -1,7 +1,38 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+HTML_WRAP = '''\
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>DB Forum</title>
+    <style>
+      h1, form { text-align: center; }
+      textarea { width: 400px; height: 100px; }
+      div.post { border: 1px solid #999;
+                 padding: 10px 10px;
+                 margin: 10px 20%%; }
+      hr.postbound { width: 50%%; }
+      em.date { color: #999 }
+    </style>
+  </head>
+  <body>
+    <h1>DB Forum</h1>
+    <form method=post>
+      <div><textarea id="content" name="content"></textarea></div>
+      <div><button id="go" type="submit">Post message</button></div>
+    </form>
+    <!-- post content will go here -->
+%s
+  </body>
+</html>
+'''
 
+@app.route("/", methods=['GET'])
+def main():
+  return HTML_WRAP
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8000)
+ 
